f60bbcc68028b137defd2db80997721b3a3017f5
Ольга Кинчак < >
2018-01-04 20:54:07 +0300

    init

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+	public float speed;
+	public Collider2D timeSphere;
+
+	private bool isCanMove;
+	private Rigidbody2D _rigidbody2D;
+	private Collider2D _collider2D;
+
+	
+	// Use this for initialization
+	void Start ()
+	{
+		isCanMove = false;
+		_rigidbody2D = GetComponent<Rigidbody2D>();
+		_collider2D = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		isCanMove = _collider2D.bounds.Intersects(timeSphere.bounds);
+		
+		
+		if (isCanMove) _rigidbody2D.velocity = new Vector2(-1 * speed, _rigidbody2D.velocity.y);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayerController : MonoBehaviour
+{
+	public float speed;
+	
+	private Vector2 direction;
+	private bool grounded;
+	private Rigidbody2D _rigidbody2D;
+
+	private void Start()
+	{
+		direction = Vector2.right; //right
+		_rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		int test = LayerMask.NameToLayer("Ground");
+		int test2 = 1 << LayerMask.NameToLayer("Ground");
+		grounded = Physics2D.Linecast(transform.position, new Vector2(transform.position.x, transform.position.y - 3f), 1 << LayerMask.NameToLayer("Ground"));  
+
+		if (Input.GetButtonDown("Jump") && grounded)
+		{
+			_rigidbody2D.AddForce(transform.up*10f, ForceMode2D.Impulse);
+		}
+	}
+	
+	private void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+
+		_rigidbody2D.velocity = new Vector2(moveHorizontal * speed, _rigidbody2D.velocity.y);
+
+	}
+}
