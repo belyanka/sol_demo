3e811d5f1ca7d96f327954bc578dc31a24ca39b6
Ольга <belyankamew@gmail.com>
2018-01-17 16:48:40 +0300

    some shit

---
+++ Assets/Scripts/MovingBackground.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBackground : MonoBehaviour {
+
+	public float scrollSpeed;
+	public bool repeated;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
