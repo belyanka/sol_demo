dc75f0a0a19c60632b4ecda555eba0ea22f9f3d7
morozovvd <v.d.morozov@yandex.ru>
2018-01-29 23:02:45 +0300

    door inverting

--- Assets/Scripts/DoorController.cs
+++ Assets/Scripts/DoorController.cs
@@ -8,23 +8,26 @@ using UnityEngine.UI;
 public class DoorController : TimeStopable, IButtonSubscriber
 {
     public List<ButtonController> buttons;
+    public bool isClosedInitially = true;
+    public bool isInverted;
 
     private Sprite _sprite;
     private int _pressedButtonsNum;
-    
 
+
+    // Use this for initialization
     private void Awake()
     {
         _pressedButtonsNum = 0;
         _sprite = GetComponentInChildren<SpriteRenderer>().sprite;
+
+        if (!isClosedInitially) Open();
     }
 
-    // Use this for initialization
+    // Use this for binding after init
     void Start()
     {
-        
-        if(buttons != null)buttons.ForEach(b => b.Subscribe(this));    
-        
+        if (buttons != null) buttons.ForEach(b => b.Subscribe(this));
     }
 
     public void Open()
@@ -42,12 +45,15 @@ public class DoorController : TimeStopable, IButtonSubscriber
     public void OnButtonPressed(ButtonController b)
     {
         _pressedButtonsNum++;
-        if (_pressedButtonsNum == buttons.Count) Open();
+        if (isInverted) Close();
+        else if (_pressedButtonsNum == buttons.Count)
+            Open();
     }
 
     public void OnButtonReleased(ButtonController b)
     {
         _pressedButtonsNum--;
-        Close();
+        if (isInverted && _pressedButtonsNum == 0) Open();
+        else Close();
     }
 }
