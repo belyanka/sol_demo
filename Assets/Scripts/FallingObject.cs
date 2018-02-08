cd7b11dcce4ad5e7dfd84bf997b98041a6ec311b
belyanka <belyankamew@gmail.com>
2018-02-09 01:05:04 +0300

    trigger button + box-sphere interaction fix

--- Assets/Scripts/FallingObject.cs
+++ Assets/Scripts/FallingObject.cs
@@ -6,34 +6,73 @@ public class FallingObject : TimeStopable {
 
 	private Rigidbody2D rb;
 	private bool inSphere;
+	private bool needSave = true;
+	private bool needRestore = true;
 	private float savedGravity;
 	private Vector2 savedVelocity;
 
 	private void Awake() {
 		TimeSphere = GameObject.FindGameObjectWithTag("TimeSphere").GetComponent<Collider2D>();
 		rb = GetComponent<Rigidbody2D>();
-		inSphere = false;
+		SaveState();
+	}
+
+	private void Start()
+	{
+		inSphere = IsCanMove();
 	}
 
 	void FixedUpdate() {
-		if (!IsCanMove()) {
-			if (!inSphere) {
-				savedVelocity = rb.velocity;
-				savedGravity = rb.gravityScale;
+		/*if (!IsCanMove()) {
+			if (inSphere) {
+				SaveState();
 				rb.gravityScale = 0;
 				rb.velocity=Vector2.zero;
 				rb.Sleep();
-				inSphere = true;
-			}
-		}
-		else {
-			if (inSphere) {
-				rb.velocity = savedVelocity;
-				rb.gravityScale = savedGravity;
-				rb.WakeUp();
 				inSphere = false;
 			}
 		}
+		else {
+			if (!inSphere) {
+				RestoreState();
+				rb.WakeUp();
+				inSphere = true;
+			}
+		}*/
+
+		if (IsCanMove()) {
+			if (inSphere) {
+				RestoreState ();
+				rb.WakeUp ();
+				//needRestore = false;
+				//needSave = true;
+				inSphere = false;
+			}
+		} else {
+			if (!inSphere) {
+				SaveState();
+				//needSave = false;
+				//needRestore = true;
+				inSphere = true;
+			}
+			Freeze();
+		}
+	}
+
+	public void SaveState() {
+		savedVelocity = rb.velocity;
+		savedGravity = rb.gravityScale;
+	}
+
+	public void RestoreState() {
+		rb.velocity = savedVelocity;
+		rb.gravityScale = savedGravity;
+	}
+
+	public void Freeze() {
+		rb.gravityScale = 0;
+		rb.velocity=Vector2.zero;
+		rb.Sleep();
 	}
 
 	public void ResetStatus() {
