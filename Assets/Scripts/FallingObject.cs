e110bb4971896e4e52a975bb4bf1dfd8894d90d1
belyanka <belyankamew@gmail.com>
2018-02-09 21:04:12 +0300

    two boxes... but why?..

--- Assets/Scripts/FallingObject.cs
+++ Assets/Scripts/FallingObject.cs
@@ -6,56 +6,35 @@ public class FallingObject : TimeStopable {
 
 	private Rigidbody2D rb;
 	private bool inSphere;
-	private bool needSave = true;
-	private bool needRestore = true;
 	private float savedGravity;
 	private Vector2 savedVelocity;
 
 	private void Awake() {
 		TimeSphere = GameObject.FindGameObjectWithTag("TimeSphere").GetComponent<Collider2D>();
 		rb = GetComponent<Rigidbody2D>();
-		SaveState();
 	}
 
 	private void Start()
 	{
+		//todo: проверить и задокументировать логику флага, назвать его как следует
+		//todo: может быть заменить на enum
 		inSphere = IsCanMove();
+		SaveState();
 	}
 
 	void FixedUpdate() {
-		/*if (!IsCanMove()) {
-			if (inSphere) {
-				SaveState();
-				rb.gravityScale = 0;
-				rb.velocity=Vector2.zero;
-				rb.Sleep();
-				inSphere = false;
-			}
-		}
-		else {
-			if (!inSphere) {
-				RestoreState();
-				rb.WakeUp();
-				inSphere = true;
-			}
-		}*/
-
 		if (IsCanMove()) {
 			if (inSphere) {
 				RestoreState ();
 				rb.WakeUp ();
-				//needRestore = false;
-				//needSave = true;
 				inSphere = false;
 			}
 		} else {
 			if (!inSphere) {
 				SaveState();
-				//needSave = false;
-				//needRestore = true;
+				Freeze();
 				inSphere = true;
 			}
-			Freeze();
 		}
 	}
 
